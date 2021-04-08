f96e48939e55558f62f4147250af859b43b66a6c
Eduardo Almeida Pontes <eapontes@bbmapfre.com.br>
2021-04-08 11:20:31 -0300

    getPost Cliete

---
+++ Documents/workspace-spring-tool-suite-4-4.10.0.RELEASE/algacrm-api2/src/main/java/com/algaworks/crm/AlgacrmApi2Application.java
@@ -0,0 +1,13 @@
+package com.algaworks.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgacrmApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgacrmApi2Application.class, args);
+	}
+
+}
